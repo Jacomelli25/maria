ede03f72d567a15a1fc9b61e17a8ad6e157fe54f
Jacomelli25 <128715807+Jacomelli25@users.noreply.github.com>
2024-03-25 09:25:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-const botoes= document.querySelectorAll(".botao"); 
+const botoes=document.querySelectorAll(".botao"); 
+const botoes=document.querySelectorAll(".botao"); 
 for(let i=0;i,botoes.lenght;i++){
   botoes[i].onclick=function(){
+
+    for(let) j=0;,botoes.lenght;j++{
+      botoes[j].classlist.remove("ativo");
+      textos[j].classlist.remove("ativo");
+    }
+     botoes[j].classlist.add("ativo");
+     textos[j].classlist.add("ativo");
+  }
+}
+const contadores=document.querySelectorAll(".contador");
+const tempoObjetivo1= new Date("2024-03-25T00:00:00")
+let tempoAtual= new Date( );
+
+contadores[0].textContent =tempoObjetivo1-tempoAtual;
